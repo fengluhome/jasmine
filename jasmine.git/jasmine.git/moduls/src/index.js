6fccd21be538f495c1c3d8e009ed7383195921d6
fengluhome <fengluhome@163.com>
2015-06-07 22:25:37 +0800

    行为测试开发

---
+++ jasmine.git/jasmine.git/moduls/src/index.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="../../Scripts/boot.js" />
+/// <reference path="../../Scripts/console.js" />
+/// <reference path="../../Scripts/jasmine-html.js" />
+/// <reference path="../../Scripts/jasmine.js" />
+/// <reference path="index.js" />
